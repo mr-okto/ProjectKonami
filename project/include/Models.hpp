b84def424df6cdf700eafc65ff09388aa190bac8
Myshonkov Maxim <maxim.myshonkov@gmail.com>
2020-04-21 09:10:00 +0300

    Add real models

--- project/include/Models.hpp
+++ project/include/Models.hpp
@@ -4,47 +4,78 @@
 #include <cstdint>
 #include <vector>
 #include <string>
+#include <Wt/Dbo/Dbo.h>
+
+namespace dbo = Wt::Dbo;
 
 class DialogueModel;
 class MessageModel;
 class PictureModel;
 class UserModel;
 
-#define DialogueModelPtr DialogueModel*
-#define MessageModelPtr MessageModel*
-#define PictureModelPtr PictureModel*
-#define UserModelPtr UserModel*
+#define DialogueModelPtr dbo::ptr<DialogueModel>
+#define MessageModelPtr dbo::ptr<MessageModel>
+#define PictureModelPtr dbo::ptr<PictureModel>
+#define UserModelPtr dbo::ptr<UserModel>
+#define IdType long long
 
 
 class DialogueModel {
  public:
-  uint32_t id_;
-  std::vector<UserModelPtr> members_;
-  std::vector<MessageModelPtr> messages_;
+  dbo::collection<UserModelPtr> members_;
+  dbo::collection<MessageModelPtr> messages_;
+  template<class Action>
+  void persist(Action& a) {
+    dbo::hasMany(a, messages_, dbo::ManyToOne, "dialogue");
+    dbo::hasMany(a, members_, dbo::ManyToMany, "dialogue_members");
+  }
 };
 
 class MessageModel {
  public:
-  uint32_t id_;
   std::string text_;
   std::time_t creation_dt_;
+  bool is_read_;
   DialogueModelPtr dialogue_;
   UserModelPtr author_;
+
+  template<class Action>
+  void persist(Action& a) {
+    dbo::field(a, text_, "text");
+    dbo::field(a, creation_dt_, "creation_dt");
+    dbo::field(a, is_read_, "is_read");
+    dbo::belongsTo(a, dialogue_, "dialogue");
+    dbo::belongsTo(a, author_, "author");
+  }
 };
 
 class PictureModel {
  public:
-  uint32_t id_;
   std::string path_;
-  uint32_t access_lvl_;
+  int access_lvl_;
   UserModelPtr user_;
+
+  template<class Action>
+  void persist(Action& a) {
+    dbo::field(a, path_, "path");
+    dbo::field(a, access_lvl_, "access_lvl");
+    dbo::belongsTo(a, user_, "user");
+  }
 };
 
 class UserModel {
  public:
-  uint32_t id_;
+
   std::string username_;
   std::string pwd_hash_;
-  std::vector<DialogueModelPtr> dialogues_;
-  std::vector<PictureModelPtr> pictures_;
+  dbo::collection<DialogueModelPtr> dialogues_;
+  dbo::collection<PictureModelPtr> pictures_;
+
+  template<class Action>
+  void persist(Action& a) {
+    dbo::field(a, username_, "username");
+    dbo::field(a, pwd_hash_, "pwd_hash");
+    dbo::hasMany(a, dialogues_,  dbo::ManyToMany, "dialogue_members");
+    dbo::hasMany(a, pictures_, dbo::ManyToOne, "user");
+  }
 };
