f333b7e5b6d2cc6078e1a62ab23c715bcce9de41
Myshonkov Maxim <maxim.myshonkov@gmail.com>
2020-04-25 22:16:00 +0300

    Add real session

--- project/include/DbSession.hpp
+++ project/include/DbSession.hpp
@@ -1,53 +1,54 @@
 #pragma once
 
 #include <memory>
+#include <Models.hpp>
+
+namespace dbo = Wt::Dbo;
 
 template< class DBConnector >
 class DbSession {
  private:
-  std::unique_ptr<DBConnector> db_connector_;
+  dbo::Session db_session_;
  public:
-  DbSession() = default;
+  DbSession(const std::string &host, uint32_t port, const std::string &user,
+            const std::string &password, const std::string &db_name);
   DbSession(const DbSession &) = delete;
   ~DbSession();
-  explicit DbSession(std::unique_ptr<DBConnector> db_connector);
-  bool connect(const std::string &host, uint32_t port, const std::string &user,
-               const std::string &password, const std::string &db_name);
-  void disconnect();
+  template<class C>
+  dbo::ptr<C> add(std::unique_ptr<C> ptr);
 };
-
-template< class DBConnector >
-DbSession<DBConnector>::DbSession(std::unique_ptr<DBConnector> db_connector)
-    : db_connector_(std::move(db_connector)) {}
-
-template< class DBConnector >
-bool DbSession<DBConnector>::connect(const std::string &host, uint32_t port,
-                                     const std::string &user, const std::string &password,
-                                     const std::string &db_name) {
-  if (!db_connector_) {
-    return false;
-  }
-  unsigned long len_limit = host.size() + user.size() + password.size() + db_name.size() + 64;
-  auto buf = new char[len_limit];
-  snprintf(buf, sizeof(buf), "host=%s user=%s password=%s port=%u dbname=%s",
-           host.c_str(), user.c_str(), password.c_str(), port, db_name.c_str());
-  bool result = db_connector_->connect(buf);
-  delete[] buf;
-  return result;
-}
-
-template< class DBConnector >
-void DbSession<DBConnector>::disconnect() {
-  if (db_connector_) {
-    db_connector_->disconnect();
-  }
-}
 
 template<class DBConnector>
 DbSession<DBConnector>::~DbSession() {
-  if (db_connector_) {
-    db_connector_->disconnect();
-  }
+    db_session_.flush();
 }
 
+template<class DBConnector>
+DbSession<DBConnector>::DbSession(const std::string &host,
+                                  uint32_t port,
+                                  const std::string &user,
+                                  const std::string &password,
+                                  const std::string &db_name) {
+  unsigned long len_limit = host.size() + user.size() + password.size() + db_name.size() + 64;
+  auto buf = new char[len_limit];
+  snprintf(buf, len_limit, "host=%s user=%s password=%s port=%u dbname=%s",
+           host.c_str(), user.c_str(), password.c_str(), port, db_name.c_str());
+  std::unique_ptr<DBConnector> db_connector{new DBConnector()};
+  db_connector->connect(buf);
+  delete[] buf;
+  db_session_.setConnection(std::move(db_connector));
+  db_session_.mapClass<DialogueModel>("dialogue");
+  db_session_.mapClass<MessageModel>("message");
+  db_session_.mapClass<UserModel>("user");
+  db_session_.mapClass<PictureModel>("picture");
+}
+
+template<class DBConnector>
+template<class C>
+dbo::ptr<C> DbSession<DBConnector>::add(std::unique_ptr<C> ptr) {
+  dbo::Transaction transaction(db_session_);
+  auto result = db_session_.add(std::move(ptr));
+  transaction.commit();
+  return result;
+}
 
