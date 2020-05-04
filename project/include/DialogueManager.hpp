c9e97d58179b84dc8a5cd6869acfa67569ca4e5f
Myshonkov Maxim <maxim.myshonkov@gmail.com>
2020-05-04 17:40:00 +0300

    Add dialogue query

--- project/include/DialogueManager.hpp
+++ project/include/DialogueManager.hpp
@@ -8,7 +8,7 @@ class DialogueManager {
  public:
   DbSession<DBConnector> &db_session_;
   explicit DialogueManager(DbSession<DBConnector> &db_session);
-  DialogueModelPtr get_dialogue(uint32_t member_a, uint32_t member_b);
+  DialogueModelPtr get_dialogue(IdType member_a, IdType member_b);
 };
 
 template < class DBConnector >
@@ -16,6 +16,14 @@ DialogueManager<DBConnector>::DialogueManager(DbSession<DBConnector> &db_session
     : db_session_(db_session) {}
 
 template < class DBConnector >
-DialogueModelPtr DialogueManager<DBConnector>::get_dialogue(uint32_t member_a, uint32_t member_b) {
-  return nullptr;
+DialogueModelPtr DialogueManager<DBConnector>::get_dialogue(IdType member_a, IdType member_b) {
+  auto new_dialogue = std::make_unique<DialogueModel>(DialogueModel());
+  db_session_.start_transaction();
+  UserModelPtr member_ptr = db_session_.template get_by_id<UserModel>(member_a);
+  new_dialogue->members_.insert(member_ptr);
+  member_ptr = db_session_.template get_by_id<UserModel>(member_b);
+  new_dialogue->members_.insert(member_ptr);
+  DialogueModelPtr result = db_session_.add(std::move(new_dialogue));
+  db_session_.commit_transaction();
+  return result;
 }
