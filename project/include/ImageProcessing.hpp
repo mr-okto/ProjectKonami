3db9684603e1a1fe8e4ebab87b0b61480882fe37
Myshonkov Maxim <maxim.myshonkov@gmail.com>
2020-05-11 01:10:15 +0300

    Improve image copies naming

--- project/include/ImageProcessing.hpp
+++ project/include/ImageProcessing.hpp
@@ -1,7 +1,9 @@
 #pragma once
 #include <opencv4/opencv2/opencv.hpp>
 
-void process_image(const std::string &input_path, const std::string &output_path, unsigned int blur_lvl);
+bool process_image(const std::string &input_path, const std::string &output_path, unsigned int blur_lvl);
 cv::Mat gaussian_blur(const cv::Mat &image, int m_dim);
-std::vector<std::string> create_blurred_copies(const std::string &image_path, const std::string &dest_dir,
-                                               long long user_id);
+std::vector<std::string> create_blurred_copies(const std::string &image_path,
+                                               const std::string &dest_dir,
+                                               long long user_id,
+                                               unsigned int blur_levels);
