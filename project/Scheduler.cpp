6ab5170cf14224b6e469c76a52c72788fc627984
Alexey Nadtochiy <nadtochiyalex@mail.ru>
2020-05-31 23:05:34 +0300

    only remove files

--- project/Scheduler.cpp
+++
@@ -1,7 +0,0 @@
-#include "Scheduler.hpp"
-
-void Scheduler::add_task(Clock::time_point time, std::shared_ptr<Task> t) {
-    std::lock_guard<std::mutex> l(lock_);
-    tasks_.emplace(time, std::move(t));
-    sleeper_.interrupt();
-}
